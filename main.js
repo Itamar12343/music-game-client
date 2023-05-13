0cceb05f8425ac5bfbbf915debc085772b13dc48
itamar hess <Itamardarihess@gmail.com>
2023-05-13 17:12:35 +0300

    count down animation

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const input = el(".input");
 const join_btn = el(".join-btn");
 const input_text = el(".input-text");
 const title = el(".title");
+const count_down = el(".count-down");
 let room = false;
 
 function el(element) {
@@ -27,13 +28,8 @@ function eventListeners() {
     join_btn.onclick = () => {
         if (room === true) {
             socket.emit("join room", input.value);
-            input_box.style.transform = "translate(-50%,-50%) scale(1.2)";
-            setTimeout(() => {
-                input_box.style.transform = "translate(-50%,-50%) scale(0)";
-                setTimeout(() => {
-                    title.style.opacity = "1";
-                }, 600);
-            }, 300);
+            hideInput();
+            toggleTitle(true);
         }
     }
 }
@@ -62,15 +58,19 @@ function socketListeners() {
         }
 
         if (data.numberOfUsers === 2) {
-            input_text.textContent = ` הצתרפות למשחק ${data.room}`;
+            input_text.textContent = `הצתרפות למשחק`;
             join_btn.textContent = "הצתרף";
         }
         if (data.numberOfUsers === 1) {
-            input_text.textContent = ` יצירת משחק ${data.room}`;
+            input_text.textContent = ` יצירת משחק`;
             join_btn.textContent = "צור משחק";
         }
     });
 
+    socket.on("the connection started", () => {
+        count_down_animation();
+        toggleTitle(false);
+    });
 }
 
 function listenToModes() {
@@ -105,6 +105,57 @@ function goToGame() {
             before_load_box.style.transform = "scale(0)";
         }, 300);
     }, 500);
+}
+
+function hideInput() {
+    input_box.style.transform = "translate(-50%,-50%) scale(1.2)";
+    setTimeout(() => {
+        input_box.style.transform = "translate(-50%,-50%) scale(0)";
+    }, 300);
+}
+
+function toggleTitle(toggle) {
+    setTimeout(() => {
+        if (toggle === true) {
+            title.style.opacity = "1";
+        } else {
+            title.style.opacity = "0";
+        }
+    }, 600);
+}
+
+function count_down_animation() {
+    setTimeout(() => {
+        count_down.style.transform = "translate(-50%,-50%) scale(1.2)";
+        count_down_animation_part(2, "#93c5ff");
+        setTimeout(() => {
+            count_down_animation_part(1, "#ff91dc");
+            setTimeout(() => {
+                count_down_animation_part(0, "#b2ff9a");
+            }, 1000);
+        }, 1000);
+    }, 600);
+}
+
+function count_down_animation_part(number, color) {
+    setTimeout(() => {
+        count_down.style.transform = "translate(-50%,-50%) scale(1)";
+        setTimeout(() => {
+            count_down.style.transform = "translate(-50%,-50%) scale(0.6)";
+            count_down.style.backgroundColor = color;
+            setTimeout(() => {
+                count_down.style.transform = "translate(-50%,-50%) scale(1)";
+                count_down.textContent = number;
+                setTimeout(() => {
+                    if (number === 0) {
+                        count_down.style.transform = "translate(-50%,-50%) scale(0)";
+                    } else {
+                        count_down.style.transform = "translate(-50%,-50%) scale(0.6)";
+                    }
+                }, 800);
+            }, 200);
+        }, 400);
+    }, 400);
 }
 
 function btnAnimation(btn) {
